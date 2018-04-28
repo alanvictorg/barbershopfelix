bdd3ec16a00fa714134377f530938da3b979d3ff
alanvictorg <alanvictorg@hotmail.com>
2018-04-27 23:34:31 -0300

    Primeiro commit

---
+++ resources/views/users/_form.blade.php
@@ -0,0 +1,25 @@
+<div class="col-md-12">
+    <div class="form-group{{ $errors->has("name") ? ' has-error' : '' }}">
+        {!! Form::label("name", 'Nome', ['class' => '']) !!}
+        {!! Form::text("name", null, ["class" => "form-control", 'id'=>'name','required','placeholder'=>"Digite um Nome",'required'])  !!}
+        <small class="text-danger">{{ $errors->first("name") }}</small>
+    </div>
+</div>
+
+<div class="col-md-12">
+    <div class="form-group{{ $errors->has("email") ? ' has-error' : '' }}">
+        {!! Form::label("email", 'Email', ['class' => '']) !!}
+        {!! Form::email("email", null, ["class" => "form-control", 'id'=>'email','required','placeholder'=>"Digite um email",'required'])  !!}
+        <small class="text-danger">{{ $errors->first("email") }}</small>
+    </div>
+</div>
+<div class="col-md-12">
+    <div class="form-group{{ $errors->has("imagepath") ? ' has-error' : '' }}">
+        {!! Form::label("imagepath", 'Foto', ['class' => '']) !!}
+        {!! Form::file("imagepath", ['class' => 'form-control']) !!}
+        <small class="text-danger">{{ $errors->first("imagepath") }}</small>
+    </div>
+</div>
+
+
+
---
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -0,0 +1,77 @@
+@extends('adminlte::layouts.auth')
+
+@section('htmlheader_title')
+    Entrar
+@endsection
+
+@section('content')
+    <body class="hold-transition login-page">
+    <div id="app">
+        <div class="login-box">
+            <div class="login-logo">
+                <b>BarberShop</b>Félix
+                <img src="img/barbershop.png">
+            </div><!-- /.login-logo -->
+
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <div class="login-box-body">
+                <form action="{{ url('/login') }}" method="post">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="form-group has-feedback">
+                        <input type="email" class="form-control"
+                               placeholder="Email" name="email"/>
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control"
+                               placeholder="Senha" name="password"/>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <div class="checkbox icheck">
+                                <label>
+                                    <input type="checkbox"
+                                           name="remember"> Lembrar-me
+                                </label>
+                            </div>
+                        </div><!-- /.col -->
+                        <div class="col-xs-4">
+                            <button type="submit"
+                                    class="btn btn-primary btn-block btn-flat">Entrar</button>
+                        </div><!-- /.col -->
+                    </div>
+                </form>
+
+                <a href="{{ url('/password/reset') }}">Esqueci a senha</a><br>
+                <a href="{{ url('/register') }}"
+                   class="text-center">Cadastrar</a>
+
+            </div><!-- /.login-box-body -->
+
+        </div><!-- /.login-box -->
+    </div>
+    @include('adminlte::layouts.partials.scripts_auth')
+
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    </script>
+    </body>
+
+@endsection
