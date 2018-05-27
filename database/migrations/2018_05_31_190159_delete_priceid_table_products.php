6af62e42fbb19a788a8156c38dc1d1bf749ce22f
Alan <alanvictorg@hotmail.com>
2018-05-27 20:01:15 -0300

    Alterando migration

--- database/migrations/2018_05_31_190159_delete_priceid_table_products.php
+++ database/migrations/2018_05_31_190159_delete_priceid_table_products.php
@@ -14,6 +14,7 @@ class DeletePriceidTableProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_price_id_foreign');
             $table->integer('price_id')->nullable()->change();
         });
     }
